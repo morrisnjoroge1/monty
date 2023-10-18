e3dd52d8d982fb22c7ed26817df449fe368749ac
morris <mnjoroge25439@gmail.com>
2023-10-17 20:49:08 -0700

    stack:operations

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+*f_nop- function that does nothing
+*@head:  head stack
+*@counter: number of elements
+*Return: no return
+*/
+void f_nop(stack_t **head, unsigned int counter)
+{
+(void) counter;
+(void) head;
+}
