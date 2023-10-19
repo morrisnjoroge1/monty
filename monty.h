239842a5623d5131bc2a58e936fbccf364ba8084
morris <mnjoroge25439@gmail.com>
2023-10-19 13:27:20 -0700

    challenges

--- monty.h
+++ monty.h
@@ -43,7 +43,6 @@ void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 extern stack_t *head;
-stack_t *head = NULL;
 typedef void (*op_func)(stack_t **, unsigned int);
 
 /*ALL THE PROTOTYPES*/
